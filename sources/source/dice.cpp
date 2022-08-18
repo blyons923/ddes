facbf228a12fbe5a2b5ba73d0a4c309bf5846ddd
Bowen Lyons <111466498+blyons923@users.noreply.github.com>
2022-08-17 21:54:06 -0500

    Update dice.cpp

--- sources/source/dice.cpp
+++ sources/source/dice.cpp
@@ -16,7 +16,7 @@
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 */
 
-#include "sources/source/dice.h"
+#include "sources/header/dice.h"
 #include "ctime"
 #include "cstdlib"
 
